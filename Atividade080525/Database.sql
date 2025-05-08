573c890e3e4e9ff75a450f496b4383346d9b85ed
yDewolf <andrersng@gmail.com>
2025-05-08 11:32:06 -0300

    Add some comments

--- Atividade080525/Database.sql
+++ Atividade080525/Database.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE Livraria;
 USE Livraria;
 
+-- Criar a tabela autores com as informações de um autor
 CREATE TABLE Autores (
     id_autor INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     nacionalidade VARCHAR(64) NOT NULL
 );
 
+-- Criar a tabela livro contendo informações sobre o livro e uma referência para seu autor
 CREATE TABLE Livros (
     id_livro INT AUTO_INCREMENT PRIMARY KEY ,
     titulo VARCHAR(255) NOT NULL,
@@ -16,6 +18,7 @@ CREATE TABLE Livros (
     CONSTRAINT FK_IdAutor FOREIGN KEY (id_autor) REFERENCES Autores(id_autor) 
 );
 
+-- Criar a tabela cliente
 CREATE TABLE Clientes (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
@@ -23,6 +26,7 @@ CREATE TABLE Clientes (
     cidade VARCHAR(127) NOT NULL
 );
 
+-- Criar a tabela de vendas com seus devidos atributos, restrições e referências
 CREATE TABLE Vendas (
     id_venda INT AUTO_INCREMENT PRIMARY KEY,
     data_venda DATE NOT NULL,
