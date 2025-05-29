e6eb023d98741566896e4075dfbecff246ee10f7
yDewolf <andrersng@gmail.com>
2025-05-29 13:53:16 -0300

    Modulo 4

---
+++ Atividade080525/Modulo4e5.sql
@@ -0,0 +1,26 @@
+USE Livraria;
+
+-- Livros por preço
+SELECT titulo, preco
+FROM Livros
+ORDER BY preco;
+
+-- Quantidade de vendas por livro
+SELECT Livros.titulo, SUM(V.quantidade)
+FROM vendas V
+JOIN Livros ON V.id_livro = Livros.id_livro
+GROUP BY Livros.titulo;
+
+-- Média de preço de livro por autor
+SELECT AVG(Livros.preco), Autores.nome
+FROM Livros
+JOIN Autores ON Livros.id_autor = Livros.id_autor
+GROUP BY Autores.nome;
+
+-- Apenas autores que tem mais de um livro
+SELECT nome, COUNT(Livros.id_livro)
+FROM Autores
+JOIN Livros ON Livros.id_autor = Autores.id_autor
+GROUP BY Autores.nome
+HAVING COUNT(Livros.id_livro) > 1;
+
