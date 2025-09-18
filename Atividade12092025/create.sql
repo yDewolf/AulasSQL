5aa85b52cb47765e3eb03702c2bbfbe456fd13b8
yDewolf <andrersng@gmail.com>
2025-09-18 11:32:19 -0300

    Fix the views

--- Atividade12092025/create.sql
+++ Atividade12092025/create.sql
@@ -48,20 +48,20 @@ CREATE TABLE Itens_Pedido (
 
 CREATE VIEW Boletim_Pedidos AS
 SELECT 
-    P.id,
-    C.nome,
+    P.id CodigoPedido,
+    C.nome NomeCliente,
     P.data_pedido,
     P.valor_total,
     C.cidade
 FROM Pedidos P
-JOIN Clientes C ON P.id_cliente = C.id;
+INNER JOIN Clientes C ON P.id_cliente = C.id;
 
 CREATE VIEW Produto_Categoria AS
 SELECT 
     Produtos.nome NomeProduto,
     Categorias.nome NomeCategoria
 FROM Produtos
-JOIN Categorias ON Produtos.id_categoria = Categorias.id;
+INNER JOIN Categorias ON Produtos.id_categoria = Categorias.id;
 
 CREATE VIEW Vendedor_Pedido AS
 SELECT 
@@ -70,54 +70,55 @@ SELECT
     C.nome NomeCliente,
     P.data_pedido
 FROM Pedidos P
-JOIN Vendedores V ON P.id_vendedor = V.id
-JOIN Clientes C ON P.id_cliente = C.id
-GROUP BY V.id;
+INNER JOIN Vendedores V ON P.id_vendedor = V.id
+INNER JOIN Clientes C ON P.id_cliente = C.id
+GROUP BY V.id, V.nome;
 
 CREATE VIEW Cliente_MaiorCompra AS
 SELECT 
-    C.nome,
+    C.nome NomeCliente,
     P.data_pedido,
     P.valor_total
 FROM Pedidos P
 JOIN Clientes C ON P.id_cliente = C.id
-WHERE P.valor_total > 5000;
+WHERE P.valor_total >= 5000;
 
 CREATE VIEW Categoria_MaisVendida AS
 SELECT 
     Cat.nome,
-    SUM(IP.quantidade)
+    SUM(IP.quantidade) TotalVendido
 FROM Itens_Pedido IP
-JOIN Produtos P ON IP.id_produto = P.id
-JOIN Categorias Cat ON P.id_categoria = Cat.id
+INNER JOIN Produtos P ON IP.id_produto = P.id
+INNER JOIN Categorias Cat ON P.id_categoria = Cat.id
 GROUP BY Cat.nome;
 
 CREATE VIEW Cliente_TotalPedidos AS
 SELECT 
-    C.nome,
+    C.nome NomeCliente,
     C.cidade,
-    SUM(IP.quantidade)
+    SUM(IP.quantidade) TotalPedidos
 FROM Itens_Pedido IP
 INNER JOIN Pedidos P ON IP.id_pedido = P.id
 INNER JOIN Clientes C ON P.id_cliente = C.id
-GROUP BY C.nome;
+GROUP BY C.id, C.nome;
 
 CREATE VIEW Vendedor_Faturamento AS
 SELECT
-    V.nome,
-    COUNT(P.id),
-    SUM(P.valor_total)
+    V.nome NomeVendedor,
+    COUNT(P.id) QuantidadePedidos,
+    SUM(P.valor_total) ValorTotalVendas
 FROM Vendedores V
-INNER JOIN Pedidos P ON P.id_vendedor = V.id;
-GROUP BY V.nome;
+INNER JOIN Pedidos P ON P.id_vendedor = V.id
+GROUP BY V.id, V.nome;
 
--- Acho que essa aqui tá errada
--- CREATE VIEW Produtos_NaoVendidos AS
--- SELECT 
---     Prod.nome
--- FROM Produtos Prod
--- INNER JOIN Pedidos P ON P.id_produto = Prod.id;
--- WHERE P.id_cliente IS NULL;
+CREATE VIEW Produtos_NaoVendidos AS
+SELECT 
+    Prod.nome
+FROM Produtos Prod
+INNER JOIN Itens_Pedido IP ON Prod.id = IP.id_produto
+INNER JOIN Pedidos P ON IP.id_pedido = P.id
+WHERE P.data_pedido BETWEEN '2024-01-01' AND '2024-12-31' 
+AND P.id IS NULL;
 
 -- Essa aqui também parece errada
 CREATE VIEW Clientes_Premium AS
@@ -125,5 +126,6 @@ SELECT
     C.nome,
     C.cidade
 FROM Clientes C
-JOIN Pedidos P ON P.id_cliente = C.id_cliente
-WHERE COUNT(P.id) >= 10;
+INNER JOIN Pedidos P ON P.id_cliente = C.id
+GROUP BY C.id, C.nome
+HAVING COUNT(P.id) >= 10;
